3f9557c4fc31dd6d25b5d5815d78a89098d68684
Loux666 <quangdafrankie@gmail.com>
2026-05-11 10:37:03 +0700

    Update baseURL for API configuration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // baseURL: 'https://hotelbooking.click/api',
-  baseURL: 'http://hotelbooking/api',
+  baseURL: 'https://hotelbooking.click/api',
+  // baseURL: 'http://hotelbooking/api',
   withCredentials: true,
   headers: {
     Accept: 'application/json',
